86650a716cb5e4216c7b0c64d197921353d0046b
Johnson <ktjohnson@loaner5033.fullsail.com>
2022-09-12 13:47:10 -0400

    setting the global show counter variable

--- reduxCounterApp/src/components/Counter.js
+++ reduxCounterApp/src/components/Counter.js
@@ -1,13 +1,39 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: "toggle",
+    });
+  };
+  const incrementHandler = () => {
+    dispatch({
+      type: "increment",
+    });
+  };
+  const decrementHandler = () => {
+    dispatch({
+      type: "decrement",
+    });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increase by five</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
